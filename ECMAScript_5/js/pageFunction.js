539672dd5d8d4b59f8b8752b38ce6399d5cd5bf3
Андрей <andrewgolik29@gmail.com>
2018-03-05 11:11:00 +0300

    fix bug

--- ECMAScript_5/js/pageFunction.js
+++ ECMAScript_5/js/pageFunction.js
@@ -13,6 +13,7 @@ const PageFunction = (function() {
             saveBook();
         }
         select = document.getElementById('selectTypeBook');
+        drawTable();
     };
 
     function drawTable() {
