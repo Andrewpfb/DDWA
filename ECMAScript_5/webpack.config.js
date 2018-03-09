02da10eed4997d09ee8501faa52aa259e6fdc1cf
Андрей <andrewgolik29@gmail.com>
2018-03-10 00:48:30 +0300

    add watch

--- ECMAScript_5/webpack.config.js
+++ ECMAScript_5/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
+    watch: true,
     plugins: [
         new UglifyJSPlugin(),
         new CleanWebpackPlugin(['dist']),
