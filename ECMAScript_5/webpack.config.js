2817c5f1294fa31f81aa50a6bc18af9749bfb0d3
Андрей <andrewgolik29@gmail.com>
2018-03-10 00:57:47 +0300

    remove uglify, add babel (translate code to es5)

--- ECMAScript_5/webpack.config.js
+++ ECMAScript_5/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -14,8 +13,8 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     watch: true,
+    devtool: 'source-map',
     plugins: [
-        new UglifyJSPlugin(),
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             hash: true,
@@ -27,6 +26,14 @@ module.exports = {
     ],
     module: {
         rules: [{
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
                 test: /\.css$/,
                 use: [
                     'style-loader',
