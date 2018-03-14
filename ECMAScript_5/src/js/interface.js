cd30c5ca2893cc3e1de4d6d66b3e01dfbbb2f32d
Андрей <andrewgolik29@gmail.com>
2018-03-14 08:31:35 +0300

    worker

--- ECMAScript_5/src/js/interface.js
+++ ECMAScript_5/src/js/interface.js
@@ -5,6 +5,7 @@ import 'datatables.net-bs4/css/dataTables.bootstrap4.css';
 import 'tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css';
 
 import PageFunction from './pageFunction.js';
+import { bookWorker } from './worker';
 
 require('font-awesome/css/font-awesome.css');
 require('datatables.net');
@@ -17,16 +18,15 @@ window.onload = function() {
     PageFunction.InitPage();
 }
 
-if (!!window.Worker) {
-    let worker = new Worker('./worker.js');
-    let delay = new Date() - new Date(localStorage.getItem('lastUpdate'));
-    if (delay < 60000) {
-        worker.postMessage(60000 - delay);
-    } else {
-        worker.postMessage(0);
-    };
-    worker.onmessage = function(e) {
-        $('#count').val(e.data);
-        localStorage.setItem('lastUpdate', new Date());
-    };
-}
+let blob = new Blob(['(this.onmessage=', bookWorker.toString(), ')'], { type: "text/javascript" });
+let worker = new Worker(window.URL.createObjectURL(blob));
+let delay = new Date() - new Date(localStorage.getItem('lastUpdate'));
+if (delay < 60000) {
+    worker.postMessage(60000 - delay);
+} else {
+    worker.postMessage(0);
+};
+worker.onmessage = function(e) {
+    document.getElementById('count').value = e.data;
+    localStorage.setItem('lastUpdate', new Date());
+};
--- ECMAScript_5/src/js/worker.js
+++ ECMAScript_5/src/js/worker.js
@@ -1,22 +1,24 @@
-onmessage = function(e) {
+export function bookWorker(e) {
     let delay = setTimeout(function() {
         GetCount();
         let timer = setInterval(function() {
             GetCount();
         }, 60000);
-    }, e.data)
-};
+    }, e.data);
 
-async function GetCount() {
-    const options = {
-        method: 'get'
+    function GetCount() {
+        oReq = new XMLHttpRequest();
+        oReq.open('get', 'http://localhost:2403/books', true);
+        oReq.send();
+        oReq.onreadystatechange = function() {
+            if (oReq.readyState != 4) {
+                return
+            }
+            if (oReq.status != 200) {
+                console.log('Error: ' + oReq.status + ':' + oReq.statusText);
+            } else {
+                postMessage(JSON.parse(oReq.response).length);
+            }
+        }
     }
-    try {
-        const response = await fetch('http://localhost:2403/books', options);
-        const array = await response.json();
-        postMessage(array.length);
-    } catch (err) {
-        console.log(err);
-        postMessage(err);
-    }
-}
+};
